b4ca034c0d37f04d6eb3e7bc2d88d2a91449dd3b
3xpl0d1x <zmelikguseynov@mail.ru>
2025-12-04 09:55:53 +0300

    arrays + loops
    added arrays and loops

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("number 1:");
+            Console.WriteLine("Fibbonachi array");
+            int[] fibbonachi = [0, 1, 1, 2, 3, 5, 8, 13];
+            Array.ForEach(fibbonachi, Console.WriteLine);
+            //2
+            Console.WriteLine("");
+            Console.WriteLine("number 2:");
+            Console.WriteLine("Months");
+            string[] months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+            Array.ForEach(months, Console.WriteLine);
+            //3
+            Console.WriteLine("");
+            Console.WriteLine("number 3:");
+            Console.WriteLine("Matrix");
+            int[][] matrix = [[2,3,4], [4,9,16], [8,27,64]];
+            foreach (int[] inner in matrix)
+            {
+                string a = "";
+                foreach(int value in inner)
+                {
+                    a += value.ToString()+" ";
+                }
+                Console.WriteLine(a);
+            }
+            //4
+            Console.WriteLine("");
+            Console.WriteLine("number 4:");
+            Console.WriteLine("Jagged");
+            double[][] jagged = [[1, 2, 3, 4, 5], [Math.E, Math.PI],[Math.Log10(1), Math.Log10(10), Math.Log10(100), Math.Log10(1000)]];
+            foreach (double[] inner in jagged)
+            {
+                string a = "";
+                foreach (double value in inner)
+                {
+                    a += value.ToString() + " ";
+                }
+                Console.WriteLine(a);
+            }
+
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 7, 8, 9, 10, 11, 12, 13 };
+
+            //5
+            Array.Copy(array, array2, 3);
+            Console.WriteLine("number 5:");
+            Console.WriteLine("1st array");
+            Array.ForEach(array, Console.WriteLine);
+            Console.WriteLine("");
+            Console.WriteLine("2nd array");
+            Array.ForEach(array2, Console.WriteLine);
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            //6
+            Array.Resize(ref array, array.Length*2);
+            Console.WriteLine("number 6:");
+            Console.WriteLine("1st array");
+            Array.ForEach(array, Console.WriteLine);
+            Console.WriteLine("");
+            Console.WriteLine("2nd array");
+            Array.ForEach(array2, Console.WriteLine);
+        }
+    }
+}
---
+++ loops/loops/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            string a = "";
+            int n1 = 0;
+            int n2 = 1;
+            a += $"{n1} {n2} ";
+            for (int i = 0; i < 8; i++) 
+            {
+                int next =n1+n2 ;
+                a +=$"{next} ";
+                n1 = n2;
+                n2 = next;
+            }
+            Console.WriteLine("number 1, fibbonachi:");
+            Console.WriteLine(a);
+            //2
+            a = "";
+            for (int i = 2; i <= 20; i += 2)
+            {
+                a += i.ToString() + " ";
+            }
+            Console.WriteLine("number 2, even numbers:");
+            Console.WriteLine(a);
+            //3
+            a = "";
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    a += $"{i}*{j}={i*j} ";
+                }
+                a += "\n";
+            }
+            Console.WriteLine("number 3, multiplication table:");
+            Console.WriteLine(a);
+            //4
+            string password = "qwerty";
+            string h = "";
+            Console.WriteLine("Enter password:");
+            do
+            {
+                h = Console.ReadLine();
+                
+            } 
+            while (h != password);
+            Console.WriteLine("Finally");  
+        }
+    }
+}
